55ca6b8a47dd7a3cf6352989221fcd8eb7186055
ddayzzz <wangshu214@live.cn>
2020-03-03 15:01:04 +0800

    fix a bug

--- src/utils.py
+++ src/utils.py
@@ -111,10 +111,16 @@ def exp_details(args):
     print(f'    Global Rounds   : {args.epochs}\n')
 
     print('    Federated parameters:')
-    if args.iid:
-        print('    IID')
+    if args.dataset == 'brats2018':
+        if args.balanced:
+            print('    balanced')
+        else:
+            print('    unbalanced')
     else:
-        print('    Non-IID')
+        if args.iid:
+            print('    IID')
+        else:
+            print('    Non-IID')
     print(f'    Fraction of users  : {args.frac}')
     print(f'    Local Batch size   : {args.local_bs}')
     print(f'    Local Epochs       : {args.local_ep}')
